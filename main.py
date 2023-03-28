0099990147147f7687c6d80d7bbb85585a8256d7
XavierL64 <118341547+XavierL64@users.noreply.github.com>
2023-03-28 07:05:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+# display floats with 2 digits after decimal point
+pd.options.display.float_format = '{:,.2f}'.format
+
+# Create locators for ticks on the time axis
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
